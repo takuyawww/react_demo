ef21f5a8e4b653b8228611dcac1480ec0b205868
takuyawww <wakataku.11010809@gmail.com>
2021-01-11 14:16:17 +0900

    ts.config.jsonがファイルの変更

--- front/webpack.config.js
+++ front/webpack.config.js
@@ -8,58 +8,65 @@ module.exports = {
   },
   resolve: {
     modules: ["node_modules"],
-    extensions: [".ts", ".tsx"]
+    extensions: [".ts", ".tsx", ".js"]
   },
   module: {
     rules: [
-      {
-        test: /(\.js|\.jsx)$/,
-        exclude: /node_modules/,
-        use: {
-          loader: "babel-loader",
-          options: {
-            presets: [
-              [
-                "@babel/preset-env",
-                {
-                  targets: {
-                    node: "current"
-                  }
-                }
-              ],
-              "@babel/preset-react"
-            ],
-            plugins: [
-              [
-                "babel-plugin-import",
-                {
-                  libraryName: "@material-ui/icons",
-                  libraryDirectory: "",
-                  camel2DashComponentName: false
-                }
-              ]
-            ]
-          }
-        }
-      },
-      {
-        test: /\.css$/,
-        exclude: /node_modules/,
-        use: [
-          {
-            loader: "style-loader"
-          },
-          {
-            loader: "css-loader",
-            options: {
-              localsConvention: "camelCase",
-              modules: {
-                localIdentName: "[path][name]__[local]--[hash:base64:5]"
-              }
-            }
-          }
-        ]
+      { 
+        test: /\.tsx?$/, 
+        loader: "ts-loader",
+        exclude: /node_modules/
       }
-    ]
+    ],
+    // rules: [
+    //   {
+    //     test: /(\.ts|\.tsx)$/,
+    //     exclude: /node_modules/,
+    //     use: {
+    //       loader: "ts-loader",
+    //       options: {
+    //         // presets: [
+    //         //   [
+    //         //     "@babel/preset-env",
+    //         //     {
+    //         //       targets: {
+    //         //         node: "current"
+    //         //       }
+    //         //     }
+    //         //   ],
+    //         //   "@babel/preset-react"
+    //         // ],
+    //         // plugins: [
+    //         //   [
+    //         //     "babel-plugin-import",
+    //         //     {
+    //         //       libraryName: "@material-ui/icons",
+    //         //       libraryDirectory: "",
+    //         //       camel2DashComponentName: false
+    //         //     }
+    //         //   ]
+    //         // ]
+    //       }
+    //     }
+    //   },
+    //   {
+    //     test: /\.css$/,
+    //     exclude: /node_modules/,
+    //     use: [
+    //       {
+    //         loader: "style-loader"
+    //       },
+    //       {
+    //         loader: "css-loader",
+    //         options: {
+    //           localsConvention: "camelCase",
+    //           modules: {
+    //             localIdentName: "[path][name]__[local]--[hash:base64:5]"
+    //           }
+    //         }
+    //       }
+    //     ]
+    //   }
+    // ]
   }
-};
+}
